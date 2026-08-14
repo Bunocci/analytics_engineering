b1fbb64ab0fc72a0c346716a18abe2c9b4b7cd70
Bunocci <brnkibs@gmail.com>
2026-08-14 12:34:02 +0300

    Day 2: break & continue iterations covered

---
+++ Day02/break_continue.py
@@ -0,0 +1,16 @@
+
+#1. continue statement
+digits = [12, 13, 14, 15, 16]
+for i in digits:
+    if i==14:
+        continue
+    print (i)
+
+
+
+#2. break statement
+numbers = [21, 23, 24, 25, 26]
+for i in numbers:
+    if i==24:
+        break
+    print (i)
