80b50009497b2233670304ec73b50af839dbbcd9
Bunocci <brnkibs@gmail.com>
2026-09-07 23:25:36 +0300

    Day12: create module with functions

---
+++ DAY12/sales.py
@@ -0,0 +1,9 @@
+
+def calculate_order_total(price, quantity):
+    return price * quantity
+
+def calculate_tax(amount, tax_rate):
+    return amount * tax_rate
+
+def calculate_final_amount(amount,tax): 
+    return amount + tax 
